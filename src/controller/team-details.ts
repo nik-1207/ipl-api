4eb67862fb8b0bb99ee4cfb40c8bad3fb017d287
nik-1207 <nmknikhilkashyap@gmail.com>
2021-11-17 17:09:26 +0530

    update

--- src/controller/team-details.ts
+++ src/controller/team-details.ts
@@ -1,39 +1,44 @@
 import teamDetails from "../models/team-details";
 
 interface teamType {
-    captainId: string,
-    wicketKeeperId: string
+  captainId: string;
+  wicketKeeperId: string;
 }
 
 interface playerType {
-    id: string,
-    image: string,
-    name: string,
-    nationality: string,
-    stats: {
-        matches: number,
-        runs: number,
-        wickets: number
-    }
+  id: string;
+  image: string;
+  name: string;
+  nationality: string;
+  stats: {
+    matches: number;
+    runs: number;
+    wickets: number;
+  };
 }
 
 const getTeamDetails = (teamName: string) => {
-    return  teamDetails.findOne({id:teamName},{__id:0,__v:0})
-        .then((data)=> {
-            if (data!==null){
-                return data;
-            }else{
-                throw new Error('team Id not available');
-            }
-        });
-}
+  return teamDetails
+    .findOne({ id: teamName }, { __id: 0, __v: 0 })
+    .then((data:any) => {
+      if (data !== null) {
+        return data;
+      } else {
+        throw new Error("team Id not available");
+      }
+    });
+};
 
-const setTeamDetails = (id: string, team: teamType, players: Array<playerType>) => {
-    const document = new teamDetails({
-        id: id,
-        team: team,
-        players: players,
-    })
-    return  document.save().then((data:unknown)=>data);
-}
-export {getTeamDetails, setTeamDetails}
+const setTeamDetails = (
+  id: string,
+  team: teamType,
+  players: Array<playerType>
+) => {
+  const document = new teamDetails({
+    id: id,
+    team: team,
+    players: players,
+  });
+  return document.save().then((data: unknown) => data);
+};
+export { getTeamDetails, setTeamDetails };
